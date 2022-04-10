5f90d629ab5d5b74c5c6aa2a7b036f88f277685f
kitwit87 <63923923+BobbySherman@users.noreply.github.com>
2022-04-09 20:27:32 -0500

    added jQuery animations
    and remove the under construction banner

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 // script for mobile nav
 $(".menu-toggle").click(function() {
     $(this).toggleClass("fa-times");
-    $("nav").toggleClass("open");
+    $("nav").slideToggle(function() {
+        $(this);
+    });
     $("body").toggleClass("no-scroll");
 
     $(".nav-link").click(function() {
-        $(".menu-toggle").toggleClass("fa-times");
-        $("nav").toggleClass("open");
-        $("body").toggleClass("no-scroll");
+        $(".menu-toggle").removeClass("fa-times");
+        $("nav").removeAttr("style");
+        $("body").removeClass("no-scroll");
     });
 });
